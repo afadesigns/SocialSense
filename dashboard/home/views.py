309233e36e59424aca37223bb97955d6845e5742
Andreas Fahl <afadesign.official@gmail.com>
2024-03-20 20:08:41 +0100

    Update dashboard.html and views.py

--- dashboard/home/views.py
+++ dashboard/home/views.py
@@ -8,24 +8,30 @@ from services.instagram.instagram_service import InstagramService
 def index(request):
     user = request.user
 
-    # Assuming user's Instagram credentials are stored securely
+    # Initialize the InstagramService with user's Instagram credentials
     instagram_service = InstagramService(
         user.instagram_username, user.instagram_password
     )
     context = {"instagram_error": "Could not fetch Instagram data."}
 
     if instagram_service.is_authenticated:
-        # Existing profile data and recent media fetching
+        # Fetch profile data and recent media
         profile_data = instagram_service.fetch_profile_data()
         recent_media = instagram_service.fetch_recent_media()
+
         # Fetch additional insights data
-        insights_data = instagram_service.fetch_insights()
+        insights_data = (
+            instagram_service.fetch_insights()
+        )  # Assuming this method fetches comprehensive insights
+
+        # Prepare additional insights for visualization
+        # Here you can process the insights data further if necessary, e.g., aggregating data for chart visualization
 
         # Update context with all fetched data
         context = {
             "profile_data": profile_data,
             "recent_media": recent_media,
-            "insights_data": insights_data,  # Add insights data to the context
+            "insights_data": insights_data,  # Include insights data in the context
             "instagram_error": "",  # Clear the error message upon successful data fetch
         }
 
