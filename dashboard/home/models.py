cbf8e31c089d1a2b755920c064df9dd532d67ba0
Andreas Fahl <afadesign.official@gmail.com>
2024-03-24 00:33:51 +0100

    Update encodings.xml and models.py

--- dashboard/home/models.py
+++ dashboard/home/models.py
@@ -2,6 +2,8 @@
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -10,18 +12,15 @@ class UserProfile(models.Model):
     instagram_password = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return self.user.username + "'s profile"
-
-
-# Signal to create or update the user's profile
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+        return f"{self.user.username}'s profile"
 
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
+    # Ensure a profile is created if it doesn't exist
     if created:
         UserProfile.objects.create(user=instance)
+    # Retrieve the profile instance for existing users and save it
     instance.profile.save()
 
 
