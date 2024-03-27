12b8e7afc73ef0b5e28e87538810e764ecd39fa4
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-27 23:33:09 +0100

    improve

--- services/tinder/tinderbotz/helpers/profile_helper.py
+++ services/tinder/tinderbotz/helpers/profile_helper.py
@@ -7,53 +7,69 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from tinderbotz.helpers.xpaths import content, modal_manager
 
-
 class ProfileHelper:
+    """
+    Helper class to handle Tinder profile related operations.
+    """
 
-    delay = 5
-
-    HOME_URL = "https://www.tinder.com/app/profile"
+    delay: int = 5
+    HOME_URL: str = "https://www.tinder.com/app/profile"
 
     def __init__(self, browser):
+        """
+        Initializes the ProfileHelper class with a web browser instance.
+
+        :param browser: Web browser instance
+        """
         self.browser = browser
 
         # open profile
         try:
             xpath = '//*[@href="/app/profile"]'
             WebDriverWait(self.browser, self.delay).until(
-                EC.presence_of_element_located((By.XPATH, xpath))
+                EC.visibility_of_element_located((By.XPATH, xpath))
             )
-            browser.find_element(By.XPATH, xpath).click()
-        except:
-            pass
+            self.browser.find_element_by_xpath(xpath).click()
+        except Exception as e:
+            print(f"Error while opening profile: {e}")
 
         self._edit_info()
 
     def _edit_info(self):
+        """
+        Clicks on the "Edit Info" button.
+        """
         xpath = '//a[@href="/app/profile/edit"]'
 
         try:
             WebDriverWait(self.browser, self.delay).until(
-                EC.presence_of_element_located((By.XPATH, xpath))
+                EC.visibility_of_element_located((By.XPATH, xpath))
             )
-            self.browser.find_element(By.XPATH, xpath).click()
-            time.sleep(1)
+            self.browser.find_element_by_xpath(xpath).click()
         except Exception as e:
-            print(e)
+            print(f"Error while clicking on Edit Info: {e}")
 
     def _save(self):
+        """
+        Clicks on the "Save" button.
+        """
         xpath = f"{content}/div/div[1]/div/main/div[1]/div/div/div/div/div[1]/a"
+
         try:
             WebDriverWait(self.browser, self.delay).until(
-                EC.presence_of_element_located((By.XPATH, xpath))
+                EC.visibility_of_element_located((By.XPATH, xpath))
             )
-            self.browser.find_element(By.XPATH, xpath).click()
+            self.browser.find_element_by_xpath(xpath).click()
             time.sleep(1)
         except Exception as e:
-            print(e)
+            print(f"Error while clicking on Save: {e}")
 
-    def add_photo(self, filepath):
-        # get the absolute filepath instead of the relative one
+    def add_photo(self, filepath: str):
+        """
+        Adds a photo to the profile.
+
+        :param filepath: Absolute filepath of the photo to be added
+        """
         filepath = os.path.abspath(filepath)
 
         # "add media" button
@@ -62,50 +78,43 @@ class ProfileHelper:
         )
         try:
             WebDriverWait(self.browser, self.delay).until(
-                EC.presence_of_element_located((By.XPATH, xpath))
+                EC.visibility_of_element_located((By.XPATH, xpath))
             )
-            btn = self.browser.find_element(By.XPATH, xpath)
+            btn = self.browser.find_element_by_xpath(xpath)
             self.browser.execute_script("arguments[0].scrollIntoView();", btn)
             btn.click()
         except Exception as e:
-            print(e)
+            print(f"Error while clicking on add media: {e}")
 
         xpath_input = f"{modal_manager}/div/div/div[1]/div[2]/div[2]/div/div/input"
         try:
             WebDriverWait(self.browser, self.delay).until(
-                EC.presence_of_element_located((By.XPATH, xpath_input))
+                EC.visibility_of_element_located((By.XPATH, xpath_input))
             )
-            self.browser.find_element(By.XPATH, xpath_input).send_keys(filepath)
+            self.browser.find_element_by_xpath(xpath_input).send_keys(filepath)
         except Exception as e:
-            print(e)
+            print(f"Error while entering filepath: {e}")
 
         xpath_choose = f"{modal_manager}/div/div/div[1]/div[1]/button[2]"
         try:
             WebDriverWait(self.browser, self.delay).until(
-                EC.presence_of_element_located((By.XPATH, xpath_choose))
+                EC.visibility_of_element_located((By.XPATH, xpath_choose))
             )
-            self.browser.find_element(By.XPATH, xpath_choose).click()
+            self.browser.find_element_by_xpath(xpath_choose).click()
         except Exception as e:
-            print(e)
+            print(f"Error while clicking on Choose: {e}")
 
         self._save()
 
-    def set_bio(self, bio):
+    def set_bio(self, bio: str):
+        """
+        Sets the bio of the profile.
+
+        :param bio: Bio to be set
+        """
         xpath = f"{content}/div/div[1]/div/main/div[1]/div/div/div/div/div[2]/div[2]/div/textarea"
 
         try:
             WebDriverWait(self.browser, self.delay).until(
-                EC.presence_of_element_located((By.XPATH, xpath))
-            )
-            text_area = self.browser.find_element(By.XPATH, xpath)
+                EC.visibility_of_element_located((By.XPATH, xpath))
 
-            for _ in range(500):
-                text_area.send_keys(Keys.BACKSPACE)
-
-            time.sleep(1)
-            text_area.send_keys(bio)
-            time.sleep(1)
-        except Exception as e:
-            print(e)
-
-        self._save()
