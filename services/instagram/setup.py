904a12470d1867e6e054699bb16134e2f33cae2c
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-27 23:32:14 +0100

    improve

--- services/instagram/setup.py
+++ services/instagram/setup.py
@@ -1,29 +1,6 @@
 from setuptools import find_packages, setup
 
-long_description = """
-Fast and effective Instagram Private API wrapper (public+private requests and challenge resolver).
-
-Use the most recent version of the API from Instagram.
-
-Features:
-
-1. Performs Public API (web, anonymous) or Private API (mobile app, authorized)
-   requests depending on the situation (to avoid Instagram limits)
-2. Challenge Resolver have Email (as well as recipes for automating receive a code from email) and SMS handlers
-3. Support upload a Photo, Video, IGTV, Clips (Reels), Albums and Stories
-4. Support work with User, Media, Insights, Collections, Location (Place), Hashtag and Direct objects
-5. Like, Follow, Edit account (Bio) and much more else
-6. Insights by account, posts and stories
-7. Build stories with custom background, font animation, swipe up link and mention users
-8. In the next release, account registration and captcha passing will appear
-"""
-
-requirements = [
-    "requests<3.0,>=2.25.1",
-    "PySocks==1.7.1",
-    "pydantic==2.5.3",
-    "pycryptodomex==3.20.0",
-]
+requirements = []
 # requirements = [
 #     line.strip()
 #     for line in open('requirements.txt').readlines()
@@ -66,7 +43,7 @@ setup(
         "instagram-note",
     ],
     description="Fast and effective Instagram Private API wrapper",
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
     python_requires=">=3.9",
@@ -78,5 +55,13 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
 )
+
+
+
+requests<3.0,>=2.25.1
+PySocks==1.7.1
+pydantic==2.5.3
+pycryptodomex==3.20.0
