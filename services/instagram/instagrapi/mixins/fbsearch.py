63a66a0ac0113498ba0b488855424966dfab808f
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-27 23:32:05 +0100

    improve

--- services/instagram/instagrapi/mixins/fbsearch.py
+++ services/instagram/instagrapi/mixins/fbsearch.py
@@ -7,12 +7,33 @@ from instagrapi.extractors import (
     extract_user_short,
 )
 from instagrapi.types import Hashtag, Location, Track, UserShort
-
+from instagrapi import Client
 
 class FbSearchMixin:
+    def __init__(self, client: Client):
+        self.client = client
+        self.timezone_offset = client.timezone_offset
+
     def fbsearch_places(
         self, query: str, lat: float = 40.74, lng: float = -73.94
     ) -> List[Location]:
+        """
+        Searches for places near a given location.
+
+        Parameters
+        ----------
+        query: str
+            The search query.
+        lat: float
+            The latitude of the search location.
+        lng: float
+            The longitude of the search location.
+
+        Returns
+        -------
+        List[Location]
+            A list of Location objects representing the search results.
+        """
         params = {
             "search_surface": "places_search_page",
             "timezone_offset": self.timezone_offset,
@@ -21,13 +42,26 @@ class FbSearchMixin:
             "count": 30,
             "query": query,
         }
-        result = self.private_request("fbsearch/places/", params=params)
-        locations = []
-        for item in result["items"]:
-            locations.append(extract_location(item["location"]))
+        result = self.client.private_request("fbsearch/places/", params=params)
+        if not result.get("items"):
+            return []
+        locations = [extract_location(item["location"]) for item in result["items"]]
         return locations
 
     def fbsearch_topsearch_flat(self, query: str) -> List[dict]:
+        """
+        Searches for top search results.
+
+        Parameters
+        ----------
+        query: str
+            The search query.
+
+        Returns
+        -------
+        List[dict]
+            A list of dictionaries representing the search results.
+        """
         params = {
             "search_surface": "top_search_page",
             "context": "blended",
@@ -35,67 +69,104 @@ class FbSearchMixin:
             "count": 30,
             "query": query,
         }
-        result = self.private_request("fbsearch/topsearch_flat/", params=params)
-        return result["list"]
+        result = self.client.private_request("fbsearch/topsearch_flat/", params=params)
+        return result.get("list", [])
 
     def search_users(self, query: str) -> List[UserShort]:
+        """
+        Searches for users.
+
+        Parameters
+        ----------
+        query: str
+            The search query.
+
+        Returns
+        -------
+        List[UserShort]
+            A list of UserShort objects representing the search results.
+        """
         params = {
             "search_surface": "user_search_page",
             "timezone_offset": self.timezone_offset,
             "count": 30,
             "q": query,
         }
-        result = self.private_request("users/search/", params=params)
-        return [extract_user_short(item) for item in result["users"]]
+        result = self.client.private_request("users/search/", params=params)
+        return [extract_user_short(item) for item in result.get("users", [])]
 
     def search_music(self, query: str) -> List[Track]:
+        """
+        Searches for music.
+
+        Parameters
+        ----------
+        query: str
+            The search query.
+
+        Returns
+        -------
+        List[Track]
+            A list of Track objects representing the search results.
+        """
         params = {
             "query": query,
-            "browse_session_id": self.generate_uuid(),
+            "browse_session_id": self.client.generate_uuid(),
         }
-        result = self.private_request("music/audio_global_search/", params=params)
-        return [extract_track(item["track"]) for item in result["items"]]
+        result = self.client.private_request("music/audio_global_search/", params=params)
+        return [extract_track(item["track"]) for item in result.get("items", [])]
 
     def search_hashtags(self, query: str) -> List[Hashtag]:
+        """
+        Searches for hashtags.
+
+        Parameters
+        ----------
+        query: str
+            The search query.
+
+        Returns
+        -------
+        List[Hashtag]
+            A list of Hashtag objects representing the search results.
+        """
         params = {
             "search_surface": "hashtag_search_page",
             "timezone_offset": self.timezone_offset,
             "count": 30,
             "q": query,
         }
-        result = self.private_request("tags/search/", params=params)
-        return [extract_hashtag_v1(ht) for ht in result["results"]]
+        result = self.client.private_request("tags/search/", params=params)
+        return [extract_hashtag_v1(ht) for ht in result.get("results", [])]
 
     def fbsearch_suggested_profiles(self, user_id: str) -> List[UserShort]:
+        """
+        Searches for suggested profiles.
+
+        Parameters
+        ----------
+        user_id: str
+            The ID of the user to search for suggested profiles.
+
+        Returns
+        -------
+        List[UserShort]
+            A list of UserShort objects representing the search results.
+        """
         params = {
             "target_user_id": user_id,
             "include_friendship_status": "true",
         }
-        result = self.private_request("fbsearch/accounts_recs/", params=params)
-        return result["users"]
+        result = self.client.private_request("fbsearch/accounts_recs/", params=params)
+        return result.get("users", [])
 
     def fbsearch_recent(self) -> List[Tuple[int, Union[UserShort, Hashtag, Dict]]]:
         """
-        Retrieves recently searched results
+        Retrieves recently searched results.
 
         Returns
         -------
         List[Tuple[int, Union[UserShort, Hashtag, Dict]]]
             Returns list of Tuples where first value is timestamp of searh, second is retrived result
         """
-        result = self.private_request("fbsearch/recent_searches/")
-        assert result.get("status", "") == "ok", "Failed to retrieve recent searches"
-
-        data = []
-        for item in result.get("recent", []):
-            if "user" in item.keys():
-                data.append(
-                    (item.get("client_time", None), extract_user_short(item["user"]))
-                )
-            if "hashtag" in item.keys():
-                hashtag = item.get("hashtag")
-                hashtag["media_count"] = hashtag.pop("formatted_media_count")
-                data.append((item.get("client_time", None), Hashtag(**hashtag)))
-            if "keyword" in item.keys():
-                data.append((item.get("client_time", None), item["keyword"]))
-        return data
+        result = self.client.private_request("fbsearch/recent_se
