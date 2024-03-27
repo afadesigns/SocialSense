c45410c335c6ebcf1bcfb8882fc1f772c3676c6e
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-27 23:33:03 +0100

    improve

--- services/instagram/instagrapi/.github/actions/publish-docs-with-mike/configure_git_user.sh
+++ services/instagram/instagrapi/.github/actions/publish-docs-with-mike/configure_git_user.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 NO_REPLY_SUFFIX="@users.noreply.github.com"
 
 function set_and_exit() {
-  local name="${USER_NAME:-$1}"
-  local email="${USER_EMAIL:-$2}"
+  local name="${1:-${GITHUB_ACTOR}}"
+  local email="${2:-"${name}${NO_REPLY_SUFFIX}"}"
   git config --local user.name "${name}"
   git config --local user.email "${email}"
   exit 0
@@ -16,30 +16,24 @@ function json_query() {
   jq -e "${1}" "${GITHUB_EVENT_PATH}"
 }
 
+function get_user_info() {
+  if json_query ".push.pusher" > /dev/null ; then
+    echo "::debug::Found push event pusher"
+    json_query '.push.pusher | {name, email}'
+  elif json_query ".pull_request.merged_by" > /dev/null ; then
+    echo "::debug::Found pull request event merged by"
+    json_query '.pull_request.merged_by | {login: .login, email: .login + "'"${NO_REPLY_SUFFIX}"'"} '
+  elif json_query ".sender" > /dev/null ; then
+    echo "::debug::Found pull event sender"
+    json_query '.sender | {login: .login, email: .login + "'"${NO_REPLY_SUFFIX}"'"} '
+  else
+    echo "::debug::Falling back to GITHUB_ACTOR"
+    echo "{\"name\": \"${GITHUB_ACTOR}\", \"email\": \"${GITHUB_ACTOR}${NO_REPLY_SUFFIX}\"}"
+  fi
+}
+
 if [[ "${USER_NAME}" != "" && "${USER_EMAIL}" != "" ]]; then
-  set_and_exit
+  set_and_exit "${USER_NAME}" "${USER_EMAIL}"
 fi
 
-echo "::debug::Attempting push event pusher"
-if json_query ".push.pusher" > /dev/null ; then
-  echo "::debug::Found push event pusher"
-  set_and_exit "$(json_query '.push.pusher.name')" "$(json_query '.push.pusher.email')"
-fi
-
-echo "::debug::Attempting merge merged by"
-if json_query ".pull_request.merged_by" > /dev/null ; then
-  echo "::debug::Found pull request event merged by"
-  LOGIN="$(json_query '.pull_request.merged_by.login')"
-  set_and_exit "${LOGIN}" "${LOGIN}${NO_REPLY_SUFFIX}"
-fi
-
-echo "::debug::Attempting event sender"
-if json_query ".sender" > /dev/null ; then
-  echo "::debug::Found pull event sender"
-  LOGIN="$(json_query '.sender.login')"
-  set_and_exit "${LOGIN}" "${LOGIN}${NO_REPLY_SUFFIX}"
-fi
-
-echo "::debug::Falling back to GITHUB_ACTOR"
-LOGIN="${GITHUB_ACTOR:-github_action}"
-set_and_exit "${LOGIN}" "${LOGIN}${NO_REPLY_SUFFIX}"
+get_user_info | jq -r 'to_entries | .[] | {name: .key, email: .value} | [.name, .email] |
