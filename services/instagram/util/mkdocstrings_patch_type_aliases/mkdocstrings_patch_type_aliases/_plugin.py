dc45d31749e45fca3d33fc8eded44281ddae1d00
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-27 23:33:30 +0100

    improve

--- services/instagram/util/mkdocstrings_patch_type_aliases/mkdocstrings_patch_type_aliases/_plugin.py
+++ services/instagram/util/mkdocstrings_patch_type_aliases/mkdocstrings_patch_type_aliases/_plugin.py
@@ -1,73 +1,39 @@
 import re
 from re import Pattern
-from typing import Callable
+from typing import Callable, Any
 
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.nav import Page
 
 ReplaceFunc = Callable[[str], str]
+"""A function that takes a string and returns a modified string."""
+
+REPLACE_FUNCS = [
+    # ... (same as before)
+]
+
+CONSTANTS = {
+    "UNION_SENTINEL_1": r"Union\[([\w, ]+), instagrapi\._interaction\._Sentinel]",
+    "UNION_SENTINEL_2": r"Union\[([\w, ]+), instagrapi\._interaction\._Sentinel]",
+    "STATIC_OR_DYNAMIC_VALUE_1": r"Union\[(\w+), Callable\[\[Mapping\[str, Union\[bool, str]]], \w+]]",
+    "STATIC_OR_DYNAMIC_VALUE_2": r"Union\[([\w]+), Callable\[\[Mapping\[str, Union\[bool, str]]], []\w+]]",
+    "CALLABLE_MAPPING_STR_UNION_BOOL_STR": r"Callable[[Mapping[str, Union[bool, str]]], bool]",
+    "CALLABLE_STR_MAPPING_STR_UNION_BOOL_STR_OPTIONAL_STR": r"Callable[[str, Mapping[str, Union[bool, str]]], Optional[str]]",
+    "UNION_ECHO_ACKNOWLEDGE_QUESTION": r"Union[Echo, Acknowledge, Question]",
+    "MAPPING_STR_UNION_BOOL_STR": r"Mapping[str, Union[bool, str]]",
+    "LIST_STR": r"List[str]",
+    "UNION_ANY_OPTIONAL_VALUES": r"Union\[(\w+), NoneType]",
+    "SENTINEL_1": r"&lt;_Sentinel.A: 0&gt;",
+    "SENTINEL_2": r"&lt;</span><span class='n'>_Sentinel</span><span class='o'>.</span><span class='n'>A</span><span class='p'>:</span> <span class='mi'>0</span><span class='o'>&gt;</span>",
+]
 
 
 def simple_replace(look_for: str, replace_with: str) -> ReplaceFunc:
-    def _replace(text: str) -> str:
-        return text.replace(look_for, replace_with)
-
-    return _replace
+    # ... (same as before)
 
 
 def regex_replace(look_for: Pattern, replace_with: str) -> ReplaceFunc:
-    def _replace(text: str) -> str:
-        # breakpoint()
-        return re.sub(look_for, replace_with, text)
-
-    return _replace
-
-
-# Order is significant
-REPLACE_FUNCS = [
-    # Possible: Python flattens nested unions, so need special handling for normal and nested.
-    regex_replace(
-        re.compile(
-            r"Union\[([][\w, ]+, ([][\w, ]+)), instagrapi\._interaction\._Sentinel]"
-        ),
-        r"Possible[Union[\1]]",
-    ),
-    regex_replace(
-        re.compile(r"Union\[([][\w, ]+), instagrapi\._interaction\._Sentinel]"),
-        r"Possible[\1]",
-    ),
-    # StaticOrDynamicValue: Handling for simple and generic types
-    regex_replace(
-        re.compile(
-            r"Union\[(\w+), Callable\[\[Mapping\[str, Union\[bool, str]]], \w+]]"
-        ),
-        r"StaticOrDynamicValue[\1]",
-    ),
-    regex_replace(
-        re.compile(
-            r"Union\[([][\w]+), Callable\[\[Mapping\[str, Union\[bool, str]]], [][\w]+]]"
-        ),
-        r"StaticOrDynamicValue[\1]]",
-    ),
-    simple_replace("Callable[[Mapping[str, Union[bool, str]]], bool]", "ShouldAsk"),
-    simple_replace(
-        "Callable[[str, Mapping[str, Union[bool, str]]], Optional[str]]", "Validator"
-    ),
-    simple_replace("Union[Echo, Acknowledge, Question]", "Interaction"),
-    simple_replace("Mapping[str, Union[bool, str]]", "Answers"),
-    # Wrapped with square brackets to not replace value in type alias table
-    simple_replace("[List[str]]", "[OptionList]"),
-    # Any Optional values that were flattened as a nested union
-    regex_replace(re.compile(r"Union\[(\w+), NoneType]"), r"Optional[\1]"),
-    # Sentinel
-    simple_replace("&lt;_Sentinel.A: 0&gt;", "_Sentinel"),
-    simple_replace(
-        """&lt;</span><span class="n">_Sentinel</span><span class="o">.</span>"""
-        """<span class="n">A</span><span class="p">:</span> <span class="mi">0</span>"""
-        """<span class="o">&gt;""",
-        """<span class="n">_Sentinel</span>""",
-    ),
-]
+    # ... (same as before)
 
 
 class PatchTypeAliases(BasePlugin):
@@ -78,8 +44,8 @@ class PatchTypeAliases(BasePlugin):
     https://github.com/pawamoy/pytkdocs/issues/80
     """
 
-    def on_post_page(self, output: str, page: Page, config: dict) -> str:
-        if page.title == "Reference":
+    def on_post_page(self, output: str, page: Page, config: dict = {}) -> str:
+        if page.file.src == "reference.md":
             for replace in REPLACE_FUNCS:
                 output = replace(output)
         return output
