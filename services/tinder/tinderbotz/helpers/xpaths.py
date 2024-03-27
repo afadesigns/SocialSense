17fdea54bb267b2426732d600b1f371a77a5dc9e
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-27 23:32:24 +0100

    improve

--- services/tinder/tinderbotz/helpers/xpaths.py
+++ services/tinder/tinderbotz/helpers/xpaths.py
@@ -1,3 +1,5 @@
-content = "/html/body/div[1]"  #'//div[starts-with(@id, "q-")]'
+# Content container XPath
+content_container = "/html/body/div[1]"  #'//div[starts-with(@id, "q-")]'
 
-modal_manager = "/html/body/div[2]"  #'//div[starts-with(@id, "q")]'
+# Modal manager container XPath
+modal_manager_container = "/html/body/div[2]"  #'//div[starts-with(@id, "q")]'
