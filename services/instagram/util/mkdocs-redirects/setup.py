472b748dfa7a7c21b855b2f9bac7b1d558ce3cc9
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-27 23:33:09 +0100

    improve

--- services/instagram/util/mkdocs-redirects/setup.py
+++ services/instagram/util/mkdocs-redirects/setup.py
@@ -3,39 +3,50 @@ import os
 from setuptools import find_packages, setup
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename), 'r') as file:
+        return file.read()
 
 
 setup(
-    name="mkdocs-redirects_relative_redirects",
-    version="1.0.1.b1",
-    description="A MkDocs plugin for dynamic page redirects to prevent broken links.",
-    python_requires=">=2.7",
+    name='mkdocs-redirects_relative_redirects',
+    version='1.0.1.b1',
+    description='A MkDocs plugin for dynamic page redirects to prevent broken links.',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    python_requires='>=2.7',
     install_requires=[
-        "mkdocs>=1.0.4",
+        'mkdocs>=1.0.4',
     ],
     extras_require={
-        "release": [
-            "twine==1.13.0",
+        'release': [
+            'twine==1.13.0',
         ]
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
+    url='https://github.com/mkdocs/mkdocs-redirects_relative_redirects',
+    author='MkDocs Community',
+    author_email='mkdocs@googlegroups.com',
+    license='MIT',
     packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
     entry_points={
-        "mkdocs.plugins": ["redirects = mkdocs_redirects.plugin:RedirectPlugin"]
+        'mkdocs.plugins': [
+            'redirects = mkdocs_redirects.plugin:RedirectPlugin',
+        ],
     },
 )
