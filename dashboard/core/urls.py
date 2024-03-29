1c5e2ab60d3acdfeef13d311a43c9970113ea7f2
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:00:02 +0100

    improve

--- dashboard/core/urls.py
+++ dashboard/core/urls.py
@@ -5,24 +5,10 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-<<<<<<< HEAD
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("", include("home.urls")),
-    path("admin/", admin.site.urls),
-    path("", include("admin_black_pro.urls")),
-]
-=======
     
->>>>>>> e477bb9df54388f162e97c21f2b1734b4978ad00
