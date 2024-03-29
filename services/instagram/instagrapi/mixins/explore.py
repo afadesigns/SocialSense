5eb503e32326af5d0f9c9d00539366d35daac75e
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:01:04 +0100

    improve

--- services/instagram/instagrapi/mixins/explore.py
+++ services/instagram/instagrapi/mixins/explore.py
@@ -1,8 +1,14 @@
+import requests
+
 class ExploreMixin:
     """
     Helpers for the explore page
     """
 
+    def __init__(self, session):
+        self.session = session
+
+    @property
     def explore_page(self):
         """
         Get explore page
@@ -11,35 +17,43 @@ class ExploreMixin:
         -------
         dict
         """
-        return self.private_request("discover/topical_explore/")
+        return self._private_request("discover/topical_explore/")
 
-    def report_explore_media(self, media_pk: int):
+    def report_explore_media(self, media_id: int) -> bool:
         """
         Report media in explore page. This is equivalent to the "not interested" button
 
         Parameters
         ----------
-        media_pk: int
-            Media PK
+        media_id: int
+            Media ID
+
         Returns
         -------
         bool
             True if success
         """
         params = {
-            "m_pk": media_pk,
+            "m_pk": media_id,
         }
-        result = self.private_request("discover/explore_report/", params=params)
+        result = self._private_request("discover/explore_report/", params=params)
         return result["explore_report_status"] == "OK"
 
-    def explore_page_media_info(self, media_pk: int):
+    def explore_page_media_info(self, media_id: int) -> dict:
         """
         Returns media information for a media item on the explore page
 
         This is the API call that happens when you're on the explore page
         and you click into a media item. It returns information about that media item
         like comments, likes, etc.
+
+        Returns
+        -------
+        dict
+            Media information
         """
-        return self.private_request(
-            "/v1/discover/media_metadata/", params={"media_id": media_pk}
-        )["media_or_ad"]
+        params = {
+            "media_id": media_id,
+        }
+        response = self._private_request("/v1/discover/media_metadata/", params=params)
+        media_info = response.get("media_or_ad")
