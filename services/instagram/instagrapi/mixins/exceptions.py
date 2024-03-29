d6150fbaa2f3b1bbe39d3dde4ca3810c926c7745
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:00:44 +0100

    improve

--- services/instagram/instagrapi/mixins/exceptions.py
+++ services/instagram/instagrapi/mixins/exceptions.py
@@ -1,267 +1,176 @@
 class ClientError(Exception):
-    response = None
-    code = None
-    message = ""
-
-    def __init__(self, *args, **kwargs):
-        args = list(args)
-        if len(args) > 0:
-            self.message = str(args.pop(0))
-        for key in list(kwargs.keys()):
-            setattr(self, key, kwargs.pop(key))
-        if not self.message:
+    def __init__(self, message=None, title=None, response=None, **kwargs):
+        self.title = title
+        self.response = response
+        self.code = None
+        self.message = message or ""
+        if self.message:
+            self.message = str(self.message)
+        else:
             self.message = "{title} ({body})".format(
                 title=getattr(self, "reason", "Unknown"),
                 body=getattr(self, "error_type", vars(self)),
             )
-        super().__init__(self.message, *args, **kwargs)
+        self.error_type = type(self).__name__
+        self.reason = getattr(self, "reason", "Unknown")
+        self.logout_reason = getattr(self, "logout_reason", None)
+        self.status = getattr(self, "status", None)
+        super().__init__(self.message)
         if self.response:
             self.code = self.response.status_code
 
-
-class ClientUnknownError(ClientError):
-    pass
-
-
-class WrongCursorError(ClientError):
-    message = "You specified a non-existent cursor"
-
-
-class ClientStatusFail(ClientError):
-    pass
-
-
-class ClientErrorWithTitle(ClientError):
-    pass
-
-
-class ResetPasswordError(ClientError):
-    pass
-
-
-class GenericRequestError(ClientError):
-    """Sorry, there was a problem with your request"""
-
-
 class ClientGraphqlError(ClientError):
-    """Raised due to graphql issues"""
-
+    pass
 
 class ClientJSONDecodeError(ClientError):
-    """Raised due to json decoding issues"""
-
+    pass
 
 class ClientConnectionError(ClientError):
-    """Raised due to network connectivity-related issues"""
-
+    pass
 
 class ClientBadRequestError(ClientError):
-    """Raised due to a HTTP 400 response"""
-
+    pass
 
 class ClientUnauthorizedError(ClientError):
-    """Raised due to a HTTP 401 response"""
-
+    pass
 
 class ClientForbiddenError(ClientError):
-    """Raised due to a HTTP 403 response"""
-
+    pass
 
 class ClientNotFoundError(ClientError):
-    """Raised due to a HTTP 404 response"""
-
+    pass
 
 class ClientThrottledError(ClientError):
-    """Raised due to a HTTP 429 response"""
-
+    pass
 
 class ClientRequestTimeout(ClientError):
-    """Raised due to a HTTP 408 response"""
-
+    pass
 
 class ClientIncompleteReadError(ClientError):
-    """Raised due to incomplete read HTTP response"""
-
+    pass
 
 class ClientLoginRequired(ClientError):
-    """Instagram redirect to https://www.instagram.com/accounts/login/"""
-
+    pass
 
 class ReloginAttemptExceeded(ClientError):
     pass
 
-
 class PrivateError(ClientError):
-    """For Private API and last_json logic"""
-
+    pass
 
 class NotFoundError(PrivateError):
-    reason = "Not found"
-
+    pass
 
 class FeedbackRequired(PrivateError):
     pass
 
-
 class ChallengeError(PrivateError):
     pass
 
-
 class ChallengeRedirection(ChallengeError):
-    pass
-
+        pass
 
 class ChallengeRequired(ChallengeError):
     pass
 
-
 class ChallengeSelfieCaptcha(ChallengeError):
     pass
-
 
 class ChallengeUnknownStep(ChallengeError):
     pass
 
-
 class SelectContactPointRecoveryForm(ChallengeError):
     pass
-
 
 class RecaptchaChallengeForm(ChallengeError):
     pass
 
-
 class SubmitPhoneNumberForm(ChallengeError):
     pass
-
 
 class LegacyForceSetNewPasswordForm(ChallengeError):
     pass
 
-
-class LoginRequired(PrivateError):
-    """Instagram request relogin
-    Example:
-    {'message': 'login_required',
-    'response': <Response [403]>,
-    'error_title': "You've Been Logged Out",
-    'error_body': 'Please log back in.',
-    'logout_reason': 8,
-    'status': 'fail'}
-    """
-
-
-class SentryBlock(PrivateError):
-    pass
-
-
 class RateLimitError(PrivateError):
     pass
 
-
 class ProxyAddressIsBlocked(PrivateError):
-    """Instagram has blocked your IP address, use a quality proxy provider (not free, not shared)"""
-
+    pass
 
 class BadPassword(PrivateError):
     pass
 
-
 class BadCredentials(PrivateError):
     pass
-
 
 class PleaseWaitFewMinutes(PrivateError):
     pass
 
-
 class UnknownError(PrivateError):
     pass
-
 
 class TrackNotFound(NotFoundError):
     pass
 
-
 class MediaError(PrivateError):
     pass
-
 
 class MediaNotFound(NotFoundError, MediaError):
     pass
 
-
 class StoryNotFound(NotFoundError, MediaError):
     pass
-
 
 class UserError(PrivateError):
     pass
 
-
 class UserNotFound(NotFoundError, UserError):
     pass
-
 
 class CollectionError(PrivateError):
     pass
 
-
 class CollectionNotFound(NotFoundError, CollectionError):
     pass
-
 
 class DirectError(PrivateError):
     pass
 
-
 class DirectThreadNotFound(NotFoundError, DirectError):
     pass
-
 
 class DirectMessageNotFound(NotFoundError, DirectError):
     pass
 
-
 class HashtagNotFound(NotFoundError, HashtagError):
     pass
-
 
 class LocationError(PrivateError):
     pass
 
-
 class LocationNotFound(NotFoundError, LocationError):
     pass
-
 
 class TwoFactorRequired(PrivateError):
     pass
 
-
 class HighlightNotFound(NotFoundError, PrivateError):
     pass
 
-
 class NoteNotFound(NotFoundError):
-    reason = "Not found"
-
+    pass
 
 class PrivateAccount(PrivateError):
-    """This Account is Private"""
-
+    pass
 
 class InvalidTargetUser(PrivateError):
-    """Invalid target user"""
-
+    pass
 
 class InvalidMediaId(PrivateError):
-    """Invalid media_id"""
-
+    pass
 
 class MediaUnavailable(PrivateError):
-    """Media is unavailable"""
-
+    pass
 
 class AlbumConfigureError(ClientError):
     """Exception raised when an album configuration fails."""
