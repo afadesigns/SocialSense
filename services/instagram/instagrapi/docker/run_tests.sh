e2c266ee0f131890337624215c1a4091036d4158
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:01:01 +0100

    improve

--- services/instagram/instagrapi/docker/run_tests.sh
+++ services/instagram/instagrapi/docker/run_tests.sh
@@ -4,19 +4,21 @@ set -eo pipefail
 
 BLACK_ACTION="--check"
 ISORT_ACTION="--check-only"
+FORMAT_CODE=false
 
-function usage
-{
-    echo "usage: run_tests.sh [--format-code]"
+function usage() {
+    echo "usage: run_tests.sh [-f|--format-code] [-h|--help]"
     echo ""
-    echo " --format-code : Format the code instead of checking formatting."
+    echo "-f, --format-code : Format the code instead of checking formatting."
+    echo "-h, --help : Display this help message."
     exit 1
 }
 
 while [[ $# -gt 0 ]]; do
     arg="$1"
     case $arg in
-        --format-code)
+        -f|--format-code)
+        FORMAT_CODE=true
         BLACK_ACTION="--quiet"
         ISORT_ACTION="--recursive"
         ;;
@@ -36,8 +38,15 @@ done
 
 python -m unittest tests.FakeClientTestCase tests.ClientPublicTestCase
 
-echo "Running iSort..."
-isort ${ISORT_ACTION} instagrapi
+if $FORMAT_CODE; then
+    echo "Formatting code..."
+    black ${BLACK_ACTION} instagrapi
+    isort ${ISORT_ACTION} instagrapi
+else
+    echo "Checking code formatting..."
+    black ${BLACK_ACTION} instagrapi || { echo "Black failed. Please format the code."; exit 1; }
+    isort ${ISORT_ACTION} instagrapi || { echo "Isort failed. Please format the code."; exit 1; }
+fi
 
 echo "Running flake8..."
 flake8 instagrapi --count --exit-zero --statistics
