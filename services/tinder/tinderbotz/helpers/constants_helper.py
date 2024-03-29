e14ded376ef7ce15695b61d1a13b92461d7b8cae
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:00:58 +0100

    improve

--- services/tinder/tinderbotz/helpers/constants_helper.py
+++ services/tinder/tinderbotz/helpers/constants_helper.py
@@ -65,18 +65,17 @@ class Printouts:
 Hi guys,
 
 This code is open-source and available on GitHub.
-repository: <https://github.com/frederikme/TinderBotz>
+Visit the repository here: https://github.com/frederikme/TinderBotz
 
 If you find the code useful, it would mean a lot if you can star the repository to show your appreciation.
 If you're interested in learning how to write these bots yourself,
 I will be making tutorials about python selenium automation soon.
 
-youtube_channel: <https://www.youtube.com/channel/UC1i3N9R9XYxt5Imi-auLPuA>
-tutorials that will be coming:
-1. Scraping news on websites -> For absolute beginners, as an intro to selenium. 
-2. A simplified Tinderbot -> For beginners
-3. Writing an automated chess bot to play on Chess.com using stockfish (currently a private repository). -> Advanced
+Subscribe to my YouTube channel for updates: https://www.youtube.com/channel/UC1i3N9R9XYxt5Imi-auLPuA
 
-Have a nice day,
-Frederikme
-"""
+Here are some tutorials that will be coming soon:
+1. Scraping news on websites - For absolute beginners, as an intro to selenium.
+2. A simplified Tinderbot - For beginners
+3. Writing an automated chess bot to play on Chess.com using stockfish (currently a private repository). - Advanced
+
+Have a nice day
