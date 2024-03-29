d4a8373ce561bfa208117684c776501a8934df1f
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:01:05 +0100

    improve

--- services/instagram/instagrapi/examples/next_proxy.py
+++ services/instagram/instagrapi/examples/next_proxy.py
@@ -1,10 +1,5 @@
-"""
-An example when you need to change proxy
-
-https://github.com/subzeroid/instagrapi/discussions/299
-"""
-
 import random
+import time
 
 from requests.exceptions import ProxyError
 from urllib3.exceptions import HTTPError
@@ -21,27 +16,39 @@ from instagrapi.exceptions import (
     SentryBlock,
 )
 
+PROXY_LIST = [
+    "http://username:password@147.123123.123:412345",
+    "http://username:password@147.123123.123:412346",
+    "http://username:password@147.123123.123:412347",
+]
 
-def next_proxy():
-    return random.choice(
-        [
-            "http://username:password@147.123123.123:412345",
-            "http://username:password@147.123123.123:412346",
-            "http://username:password@147.123123.123:412347",
-        ]
-    )
+def get_proxy():
+    return random.choice(PROXY_LIST)
 
+def handle_network_error(cl):
+    print("Network error, retrying with a new proxy...")
+    cl.set_proxy(get_proxy())
+    return cl
 
-cl = Client(proxy=next_proxy())
+def handle_instagram_limit_error(cl):
+    print("Instagram limit error, retrying with a new proxy in 5 minutes...")
+    time.sleep(300)
+    cl.set_proxy(get_proxy())
+    return cl
 
-try:
-    cl.login("USERNAME", "PASSWORD")
-except (ProxyError, HTTPError, GenericRequestError, ClientConnectionError):
-    # Network level
-    cl.set_proxy(next_proxy())
-except (SentryBlock, RateLimitError, ClientThrottledError):
-    # Instagram limit level
-    cl.set_proxy(next_proxy())
-except (ClientLoginRequired, PleaseWaitFewMinutes, ClientForbiddenError):
-    # Logical level
-    cl.set_proxy(next_proxy())
+def handle_logical_error(cl):
+    print("Logical error, retrying with a new proxy...")
+    cl.set_proxy(get_proxy())
+    return cl
+
+def main():
+    cl = Client(proxy=get_proxy())
+
+    for _ in range(3):
+        try:
+            cl.login("USERNAME", "PASSWORD")
+            break
+        except (ProxyError, HTTPError, GenericRequestError, ClientConnectionError) as e:
+            cl = handle_network_error(cl)
+        except (SentryBlock, RateLimitError, ClientThrottledError) as e:
+            cl = handle_instagram_limit_error(cl
