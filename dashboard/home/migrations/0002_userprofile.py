1e58f74f81844cce899eecb9047e19751484e023
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:00:04 +0100

    improve

--- dashboard/home/migrations/0002_userprofile.py
+++ dashboard/home/migrations/0002_userprofile.py
@@ -3,36 +3,3 @@
 from django.conf import settings
 from django.db import migrations, models
 
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ("home", "0001_initial"),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name="UserProfile",
-            fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                (
-                    "instagram_username",
-                    models.CharField(
-                        blank=True,
-                        help_text="Instagram username of the user.",
-                        max_length=255,
-                        null=True,
-                    ),
-                ),
-                (
-                    "instagram_password",
-                    models.CharField(
-                
