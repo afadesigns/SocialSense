09e87ef1bd39b6315c2830753c3cc76dc55d1e44
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:00:05 +0100

    improve

--- dashboard/core/asgi.py
+++ dashboard/core/asgi.py
@@ -1,27 +1,32 @@
 """
-ASGI config for core project.
+ASGI configuration for the Django project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+This module exposes the ASGI callable as a module-level variable named `application`.
 
-For more information on this file, see
+For more information, see:
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+# Import the Django ASGI application.
 from django.core.asgi import get_asgi_application
 
-import your_app.routing
+# Import the routing configuration for the WebSocket protocol.
+from your_app.routing import websocket_urlpatterns
 
+# Set the default value for the DJANGO_SETTINGS_MODULE environment variable.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
+# Create an instance of the ASGI application router.
 application = ProtocolTypeRouter({
+    # Configure the HTTP/HTTPS protocol to use the Django ASGI application.
     "http": get_asgi_application(),
+    # Configure the WebSocket protocol to use the authentication middleware
+    # and the routing configuration for the `your_app` application.
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            your_app.routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
