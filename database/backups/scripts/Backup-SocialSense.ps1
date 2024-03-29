4ee5e9e96ba1c78d3b3bdb75413ed89ef6cfe469
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:01:41 +0100

    improve

--- database/backups/scripts/Backup-SocialSense.ps1
+++ database/backups/scripts/Backup-SocialSense.ps1
@@ -2,19 +2,19 @@ $BackupDir = "C:\Users\Andreas\Projects\SocialSense\database\backups"
 $DbName = "SocialSense"
 $UserName = "postgres" # Make sure this is your PostgreSQL username
 $Date = Get-Date -Format "yyyyMMddHHmmss"
-$BackupFile = "$BackupDir\$DbName-$Date.backup"
+$BackupFile = Join-Path -Path $BackupDir -ChildPath "$DbName-$Date.backup"
 
 # Ensure backup directory exists
-If (-Not (Test-Path -Path $BackupDir)) {
-    New-Item -ItemType Directory -Path $BackupDir
+if (!(Test-Path -Path $BackupDir)) {
+    New-Item -ItemType Directory -Path $BackupDir | Out-Null
 }
 
 # Create the backup using the full path to pg_dump.exe
-& "C:\Program Files\PostgreSQL\16\bin\pg_dump.exe" -h localhost -U $UserName -F c -b -v -f $BackupFile $DbName
+& "C:\Program Files\PostgreSQL\16\bin\pg_dump.exe" -h localhost -U $UserName -Fc -b -v -f $BackupFile $DbName
 
 # Retention Policy: Keep latest 5 backups, delete the rest
 $AllBackups = Get-ChildItem -Path $BackupDir -Filter "$DbName-*.backup"
-If ($AllBackups.Count -gt 5) {
+if ($AllBackups.Count -gt 5) {
     $BackupsToDelete = $AllBackups | Sort-Object CreationTime -Descending | Select-Object -Skip 5
-    $BackupsToDelete | ForEach-Object { Remove-Item $_.FullName }
+    $BackupsToDelete | ForEach-Object { Remove-Item -Path $_.FullName -Force }
 }
