28a70880d06e0acd6ac15a55f44095b9582a372c
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:01:06 +0100

    improve

--- services/instagram/.github/actions/publish-docs-with-mike/update_docs_for_version.sh
+++ services/instagram/.github/actions/publish-docs-with-mike/update_docs_for_version.sh
@@ -2,14 +2,32 @@
 
 set -eo pipefail
 
+# Check if the user provided a new version number
+if [[ "${1}" == "" ]]; then
+  echo "Usage: $(basename "${0}") <new_version>"
+  exit 1
+fi
+
 NEW_VERSION="${1}"
-PREV_LATEST="$(mike list --json | jq --raw-output '.[] | select(.aliases == ["latest"]) | .version')"
+MIKE_CMD="mike"
+JQ_CMD="jq"
+
+# Check if mike command is installed
+if ! command -v "${MIKE_CMD}" &> /dev/null; then
+  echo "Error: mike command not found in system's PATH."
+  exit 1
+fi
+
+# Check if jq command is installed
+if ! command -v "${JQ_CMD}" &> /dev/null; then
+  echo "Error: jq command not found in system's PATH."
+  exit 1
+fi
+
+PREV_LATEST="$(mike list --json | "${JQ_CMD}" --raw-output '.[] | select(.aliases == ["latest"]) | .version')"
 
 if [[ "${PREV_LATEST}" == "" ]]; then
   echo "No previous version found using the latest alias. Nothing to retitle."
 else
-  echo "mike retitle --message \"Remove latest from title of ${PREV_LATEST}\" \"${PREV_LATEST}\" \"${PREV_LATEST}\""
-  mike retitle --message "Remove latest from title of ${PREV_LATEST}" "${PREV_LATEST}" "${PREV_LATEST}"
-fi
-echo "mike deploy --update-aliases --title \"${NEW_VERSION} (latest)\" \"${NEW_VERSION}\" \"latest\""
-mike deploy --update-aliases --title "${NEW_VERSION} (latest)" "${NEW_VERSION}" "latest"
+  # Prevent accidental modification of the previous version's title
+  echo "mike retitle --dry-run --message \"
