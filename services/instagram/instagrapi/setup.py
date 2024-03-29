f6bacdd301eb7fcab68ee6de644bf4c73d141938
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:01:03 +0100

    improve

--- services/instagram/instagrapi/setup.py
+++ services/instagram/instagrapi/setup.py
@@ -2,21 +2,8 @@ import os
 
 from setuptools import find_packages, setup
 
-long_description = f"""
-{description}
-
-Features:
-
-1. Performs Public API (web, anonymous) or Private API (mobile app, authorized)
-   requests depending on the situation (to avoid Instagram limits)
-2. Challenge Resolver have Email (as well as recipes for automating receive a code from email) and SMS handlers
-3. Support upload a Photo, Video, IGTV, Clips (Reels), Albums and Stories
-4. Support work with User, Media, Insights, Collections, Location (Place), Hashtag and Direct objects
-5. Like, Follow, Edit account (Bio) and much more else
-6. Insights by account, posts and stories
-7. Build stories with custom background, font animation, swipe up link and mention users
-8. In the next release, account registration and captcha passing will appear
-"""
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 requirements = [
     "requests<3.0,>=2.25.1",
@@ -85,4 +72,6 @@ setup(
     project_urls={
         "Documentation": "https://instagrapi.readthedocs.io/",
         "Source Code": "https://github.com/subzeroid/instagrapi",
-        "Bug Tracker": "https://github.com/subzeroid
+        "Bug Tracker": "https://github.com/subzeroid/instagrapi/issues",
+    },
+)
