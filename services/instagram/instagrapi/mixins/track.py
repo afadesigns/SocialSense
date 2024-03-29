4c3933f7ecc58f3c8b1268ce26847ed46cb078d4
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2024-03-29 21:01:07 +0100

    improve

--- services/instagram/instagrapi/mixins/track.py
+++ services/instagram/instagrapi/mixins/track.py
@@ -1,19 +1,17 @@
 import shutil
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, Union
 from urllib.parse import urlparse
 
 import requests
-
 from instagrapi.exceptions import ClientError, TrackNotFound
 from instagrapi.extractors import extract_track
 from instagrapi.types import Track
 from instagrapi.utils import json_value
 
-
 class TrackMixin:
     def track_download_by_url(
-        self, url: str, filename: str = "", folder: Path = ""
+        self, url: str, filename: str = "", folder: Union[str, Path] = ""
     ) -> Path:
         """
         Download track by URL
@@ -24,7 +22,7 @@ class TrackMixin:
             URL for a track
         filename: str, optional
             Filename for the track
-        folder: Path, optional
+        folder: str or Path, optional
             Directory in which you want to download the track,
             default is "" and will download the files to working directory
 
@@ -35,27 +33,41 @@ class TrackMixin:
         """
         url = str(url)
         fname = urlparse(url).path.rsplit("/", 1)[1].strip()
-        assert fname, """The URL must contain the path to the file (m4a or mp3)."""
-        filename = "%s.%s" % (filename, fname.rsplit(".", 1)[1]) if filename else fname
-        path = Path(folder) / filename
+        if not fname:
+            raise ValueError("The URL must contain the path to the file (m4a or mp3).")
+        ext = fname.rsplit(".", 1)[-1]
+        filename = f"{filename}.{ext}" if filename else fname
+        path = Path(folder).resolve() / filename
         response = requests.get(url, stream=True, timeout=self.request_timeout)
         response.raise_for_status()
         with open(path, "wb") as f:
             response.raw.decode_content = True
             shutil.copyfileobj(response.raw, f)
-        return path.resolve()
+        return path
 
     def _track_request(self, data: Dict[str, Any]) -> Dict:
+        """
+        Make a private request to the Instagram API for track information
+
+        Parameters
+        ----------
+        data: dict
+            Request data
+
+        Returns
+        -------
+        dict
+            Response data
+        """
         try:
             result = self.private_request("clips/music/", data)
         except ClientError as e:
             if not self.last_json:
-                kw = {
-                    k: v
-                    for k, v in data.items()
-                    if k in {"music_canonical_id", "original_sound_audio_asset_id"}
+                missing_keys = {
+                    k: v for k, v in {"music_canonical_id", "original_sound_audio_asset_id"}.items()
+                    if k not in data
                 }
-                raise TrackNotFound(**kw)
+                raise TrackNotFound(**missing_keys) from e
             raise e
         return result
 
@@ -73,11 +85,13 @@ class TrackMixin:
         Track
             An object of Track type
         """
+        if not music_canonical_id:
+            raise ValueError("music_canonical_id cannot be empty.")
         data = {
             "tab_type": "clips",
             "referrer_media_id": "",
             "_uuid": self.uuid,
-            "music_canonical_id": str(music_canonical_id),
+            "music_canonical_id": music_canonical_id,
         }
         result = self.private_request("clips/music/", data)
         track = json_value(result, "metadata", "music_info", "music_asset_info")
@@ -94,9 +108,11 @@ class TrackMixin:
 
         Returns
         -------
-        Dict
+        dict
             Raw insta response json
         """
+        if not track_id:
+            raise ValueError("track_id cannot be empty.")
         data = {
             "audio_cluster_id": track_id,
             "original_sound_audio_asset_id": track_id,
